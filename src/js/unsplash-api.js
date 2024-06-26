7ff7bafb20ec1b7787d3bdd14d57f3334920ad6d
Kyrylko <kkuzmenko1992@gmail.com>
2024-06-26 09:42:38 -0230

    added function and test

---
+++ src/js/unsplash-api.js
@@ -0,0 +1,22 @@
+
+
+export function getPhotos(query) {
+    const API_KEY = 'L6DfZAiGwsspJd4cvUrj1Vf1mjnqIGAcaSeAWdUf3Fo';
+    const BASE_URL = 'https://api.unsplash.com';
+    const END_POINT = '/search/photos';
+    
+    const params = new URLSearchParams({ 
+        client_id: API_KEY,
+        query,
+        page: 1,
+        per_page: 12,
+        orientation: 'portrait',
+    });
+    return fetch(`${BASE_URL}${END_POINT}?${params}`)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(res.status)
+            }
+            return res.json(); 
+        })
+}
